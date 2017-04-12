aacf485e8a47c7e115e687cf7ab2c821295d7344
Carl Factora <cfactora93@gmail.com>
2017-04-12 16:54:25 -0400

    in progress of cleaning up inputted html code; still needs work

--- src/Tokeniser.elm
+++ src/Tokeniser.elm
@@ -1,35 +1,115 @@
 module Tokeniser exposing (..)
 
 import Array exposing (Array, fromList, get, length)
-import String exposing (split, words, left, right, slice)
+import String exposing (words, uncons, dropLeft)
 import HtmlParser exposing (..)
 import HtmlParser.Util exposing (..)
 
+strip : String -> String -> String
+strip pat str =
+  case pat of
+    "" -> str
+    _ -> 
+      case (uncons pat, uncons str) of
+        (Just (c1,rst1), Just (c2,rst2)) ->
+          if c1 == c2
+          then strip rst1 rst2
+          else strip pat rst2
+        (_,_) -> ""
+
 myElement : String
 myElement = """
-<p>Hello! Welcome to Hermes!</p>
-<p xmlns="http://www.w3.org/1999/xhtml">Here's some code for you:</p>
-<p>(Press Space to continue!)</p>
-<div xmlns="http://www.w3.org/1999/xhtml" class="lisp">
-<pre class="lisp prettyprinted" style=""><span class="opn">(</span><span class="pun">+</span><span class="pln"> </span><span class="lit">137</span><span class="pln"> </span><span class="lit">349</span><span class="clo">)</span><span class="pln">
-</span><i><span class="lit">486</span></i><span class="pln">
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:epub="http://www.idpf.org/2007/ops" xml:lang="en" lang="en">
+<!-- Created by GNU Texinfo 5.1, http://www.gnu.org/software/texinfo/ -->
+<head>
+<title>Structure and Interpretation of Computer Programs, 2e: Dedication</title>
 
-</span><span class="opn">(</span><span class="pun">-</span><span class="pln"> </span><span class="lit">1000</span><span class="pln"> </span><span class="lit">334</span><span class="clo">)</span><span class="pln">
-</span><i><span class="lit">666</span></i><span class="pln">
+<meta name="description" content="Structure and Interpretation of Computer Programs, 2e: Dedication" />
+<meta name="keywords" content="Structure and Interpretation of Computer Programs, 2e: Dedication" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<meta name="Generator" content="texi2any" />
+<meta charset="utf-8" />
+<link href="index.xhtml#Top" rel="start" title="Top" />
+<link href="Term-Index.xhtml#Term-Index" rel="index" title="Term Index" />
+<link href="index.xhtml#SEC_Contents" rel="contents" title="Table of Contents" />
+<link href="index.xhtml#Top" rel="prev" title="Top" />
+<link href="Foreword.xhtml#Foreword" rel="next" title="Foreword" />
+<link href="UTF.xhtml#UTF" rel="prev" title="UTF" />
 
-</span><span class="opn">(</span><span class="pun">*</span><span class="pln"> </span><span class="lit">5</span><span class="pln"> </span><span class="lit">99</span><span class="clo">)</span><span class="pln">
-</span><i><span class="lit">495</span></i><span class="pln">
+<link href="css/style.css" rel="stylesheet" type="text/css" />
+<link href="css/prettify.css" rel="stylesheet" type="text/css" />
 
-</span><span class="opn">(</span><span class="pun">/</span><span class="pln"> </span><span class="lit">10</span><span class="pln"> </span><span class="lit">5</span><span class="clo">)</span><span class="pln">
-</span><i><span class="lit">2</span></i><span class="pln">
+<script src="js/jquery.min.js" type="text/javascript"></script>
+<script src="js/footnotes.js" type="text/javascript"></script>
+<script src="js/browsertest.js" type="text/javascript"></script>
+</head>
 
-</span><span class="opn">(</span><span class="pun">+</span><span class="pln"> </span><span class="lit">2.7</span><span class="pln"> </span><span class="lit">10</span><span class="clo">)</span><span class="pln">
-</span><i><span class="lit">12.7</span></i>
-</pre></div>
-<p>More code:</p>
-<div>quickSort [1..5000]</div>
-<p>Now, fuck face, go and fuck off, will you? Thanks.</p>
+<body>
+<section><span class="top jump" title="Jump to top"><a href="#pagetop" accesskey="t">⇡</a></span><a id="pagetop"></a><a id="Dedication"></a>
+<nav class="header">
+<p>
+Next: <a href="Foreword.xhtml#Foreword" accesskey="n" rel="next">Foreword</a>, Prev: <a href="UTF.xhtml#UTF" accesskey="p" rel="prev">UTF</a>, Up: <a href="index.xhtml#Top" accesskey="u" rel="prev">Top</a>   [<a href="index.xhtml#SEC_Contents" title="Table of contents" accesskey="c" rel="contents">Contents</a>]</p>
+</nav>
+<a id="Dedication-1"></a>
+<h2 class="unnumbered">Dedication</h2>
+
+<p>This book is dedicated, in respect and admiration, to the spirit that lives in
+the computer.
+</p>
+<blockquote>
+<p>“I think that it’s extraordinarily important that we in computer science keep
+fun in computing. When it started out, it was an awful lot of fun. Of course,
+the paying customers got shafted every now and then, and after a while we began
+to take their complaints seriously. We began to feel as if we really were
+responsible for the successful, error-free perfect use of these machines. I
+don’t think we are. I think we’re responsible for stretching them, setting
+them off in new directions, and keeping fun in the house. I hope the field of
+computer science never loses its sense of fun. Above all, I hope we don’t
+become missionaries. Don’t feel as if you’re Bible salesmen. The world has
+too many of those already. What you know about computing other people will
+learn. Don’t feel as if the key to successful computing is only in your hands.
+What’s in your hands, I think and hope, is intelligence: the ability to see the
+machine as more than when you were first led up to it, that you can make it
+more.”
+</p>
+<p>—Alan J. Perlis (April 1, 1922 – February 7, 1990)
+</p></blockquote>
+
+<nav class="header">
+<p>
+Next: <a href="Foreword.xhtml#Foreword" accesskey="n" rel="next">Foreword</a>, Prev: <a href="UTF.xhtml#UTF" accesskey="p" rel="prev">UTF</a>, Up: <a href="index.xhtml#Top" accesskey="u" rel="prev">Top</a>   [<a href="index.xhtml#SEC_Contents" title="Table of contents" accesskey="c" rel="contents">Contents</a>]</p>
+</nav>
+
+
+</section><span class="bottom jump" title="Jump to bottom"><a href="#pagebottom" accesskey="b">⇣</a></span><a id="pagebottom"></a>
+</body>
+</html>
 """
+
+-- <p>Hello! Welcome to Hermes!</p>
+-- <p xmlns="http://www.w3.org/1999/xhtml">Here's some code for you:</p>
+-- <p>(Press Space to continue!)</p>
+-- <div xmlns="http://www.w3.org/1999/xhtml" class="lisp">
+-- <pre class="lisp prettyprinted" style=""><span class="opn">(</span><span class="pun">+</span><span class="pln"> </span><span class="lit">137</span><span class="pln"> </span><span class="lit">349</span><span class="clo">)</span><span class="pln">
+-- </span><i><span class="lit">486</span></i><span class="pln">
+
+-- </span><span class="opn">(</span><span class="pun">-</span><span class="pln"> </span><span class="lit">1000</span><span class="pln"> </span><span class="lit">334</span><span class="clo">)</span><span class="pln">
+-- </span><i><span class="lit">666</span></i><span class="pln">
+
+-- </span><span class="opn">(</span><span class="pun">*</span><span class="pln"> </span><span class="lit">5</span><span class="pln"> </span><span class="lit">99</span><span class="clo">)</span><span class="pln">
+-- </span><i><span class="lit">495</span></i><span class="pln">
+
+-- </span><span class="opn">(</span><span class="pun">/</span><span class="pln"> </span><span class="lit">10</span><span class="pln"> </span><span class="lit">5</span><span class="clo">)</span><span class="pln">
+-- </span><i><span class="lit">2</span></i><span class="pln">
+
+-- </span><span class="opn">(</span><span class="pun">+</span><span class="pln"> </span><span class="lit">2.7</span><span class="pln"> </span><span class="lit">10</span><span class="clo">)</span><span class="pln">
+-- </span><i><span class="lit">12.7</span></i>
+-- </pre></div>
+-- <p>More code:</p>
+-- <div>quickSort [1..5000]</div>
+-- <p>Now, fuck face, go and fuck off, will you? Thanks.</p>
 
 parseHtmlString : String -> Array (String, Bool)
 parseHtmlString =
@@ -42,10 +122,11 @@ parseHtmlString =
           case identifier of
             "p" -> parseString (textContent content)
             "div" -> [(textContent content, False)]
-            _ -> []
+            "script" -> []
+            _ -> parseString (textContent content)
         _ -> []
   in
-    parse >> List.concatMap parseElement >> fromList
+    strip "<html" >> dropLeft 1 >> parse >> List.concatMap parseElement >> fromList
 
 
 parseString : String -> List ( String, Bool )

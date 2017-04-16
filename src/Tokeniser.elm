72da27dbec9593db4d621a3e8a6034dab2ec9801
lazywithclass <montecristoh@gmail.com>
2017-04-16 18:29:02 -0400

    removed comments

--- src/Tokeniser.elm
+++ src/Tokeniser.elm
@@ -88,28 +88,6 @@ Next: <a href="Foreword.xhtml#Foreword" accesskey="n" rel="next">Foreword</a>, P
 </html>
 """
 
--- <p>Hello! Welcome to Hermes!</p>
--- <p xmlns="http://www.w3.org/1999/xhtml">Here's some code for you:</p>
--- <p>(Press Space to continue!)</p>
--- <div xmlns="http://www.w3.org/1999/xhtml" class="lisp">
--- <pre class="lisp prettyprinted" style=""><span class="opn">(</span><span class="pun">+</span><span class="pln"> </span><span class="lit">137</span><span class="pln"> </span><span class="lit">349</span><span class="clo">)</span><span class="pln">
--- </span><i><span class="lit">486</span></i><span class="pln">
-
--- </span><span class="opn">(</span><span class="pun">-</span><span class="pln"> </span><span class="lit">1000</span><span class="pln"> </span><span class="lit">334</span><span class="clo">)</span><span class="pln">
--- </span><i><span class="lit">666</span></i><span class="pln">
-
--- </span><span class="opn">(</span><span class="pun">*</span><span class="pln"> </span><span class="lit">5</span><span class="pln"> </span><span class="lit">99</span><span class="clo">)</span><span class="pln">
--- </span><i><span class="lit">495</span></i><span class="pln">
-
--- </span><span class="opn">(</span><span class="pun">/</span><span class="pln"> </span><span class="lit">10</span><span class="pln"> </span><span class="lit">5</span><span class="clo">)</span><span class="pln">
--- </span><i><span class="lit">2</span></i><span class="pln">
-
--- </span><span class="opn">(</span><span class="pun">+</span><span class="pln"> </span><span class="lit">2.7</span><span class="pln"> </span><span class="lit">10</span><span class="clo">)</span><span class="pln">
--- </span><i><span class="lit">12.7</span></i>
--- </pre></div>
--- <p>More code:</p>
--- <div>quickSort [1..5000]</div>
--- <p>Now, fuck face, go and fuck off, will you? Thanks.</p>
 
 parseHtmlString : String -> Array (String, Bool)
 parseHtmlString =
